222597e6c72ff5b046d2cd0f6851b0667cca90be
acassaigne <anthony.cassaigne@gmail.com>
2020-12-11 18:50:37 +0100

    green

--- specs/cpu.async.spec.ts
+++ specs/cpu.async.spec.ts
@@ -2,55 +2,60 @@ import * as chai from 'chai';
 import 'mocha';
 
 import chaiAsPromised = require('chai-as-promised');
+
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 const THRESHOLD = 50;
 
 describe('My test suite Cpu Monitor', () => {
-  it('My unit test async', async () => { 
-    let cpuMonitor = new CpuMonitor(THRESHOLD);
-    cpuMonitor.setValue(THRESHOLD+1);
-    let serviceCpuAlert = new ServiceCpuAlert(cpuMonitor);
-    const result : Promise<boolean> = serviceCpuAlert.hasAlert()
-        
-    await expect(result).to.eventually.equal(true);
-  }),
+    it('My unit test async', async () => {
+        let cpuMonitor = new CpuMonitor(THRESHOLD);
+        cpuMonitor.setValue(THRESHOLD + 1);
+        let serviceCpuAlert = new ServiceCpuAlert(cpuMonitor);
+        const result: Promise<boolean> = serviceCpuAlert.hasAlert()
 
-  it('should not alert', async() => {
-    let cpuMonitor = new CpuMonitor(THRESHOLD);
-    cpuMonitor.setValue(THRESHOLD-1);
-    let serviceCpuAlert = new ServiceCpuAlert(cpuMonitor);
-    const result : Promise<boolean> = serviceCpuAlert.hasAlert()
-        
-    await expect(result).to.eventually.equal(false);
-  })
+        await expect(result).to.eventually.equal(true);
+    }),
+
+        it('should not alert', async () => {
+            let cpuMonitor = new CpuMonitor(THRESHOLD);
+            cpuMonitor.setValue(THRESHOLD - 1);
+            let serviceCpuAlert = new ServiceCpuAlert(cpuMonitor);
+            const result: Promise<boolean> = serviceCpuAlert.hasAlert()
+
+            await expect(result).to.eventually.equal(false);
+        })
 
 });
 
 
-
 class CpuMonitor {
-  _threshold : number;
-  
-  _value : number;
-  
-  constructor(threshold : number) {
-    this._threshold = threshold;
-  }
+    _threshold: number;
+    _value: number;
 
-  setValue(value: number) {
-    this._value = value;
-  }
-  
+    constructor(threshold: number) {
+        this._threshold = threshold;
+    }
+
+    setValue(value: number) {
+        this._value = value;
+    }
+
 }
 
 class ServiceCpuAlert {
-  hasAlert(): Promise<boolean> {
-    return  Promise.resolve(true);
-  }
-  
-  constructor(cpuMon: CpuMonitor ) {
-    
-  }
+
+    _cpuMonitor : CpuMonitor;
+
+    constructor(cpuMon: CpuMonitor) {
+        this._cpuMonitor = cpuMon;
+
+    }
+
+    hasAlert(): Promise<boolean> {
+        return Promise.resolve(this._cpuMonitor._value > this._cpuMonitor._threshold);
+    }
+
+
 }
