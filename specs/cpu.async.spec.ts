f806f4020eeba0a56aa922500517c4a81e228933
jpboignard <jp.boignard@gmail.com>
2020-12-11 18:46:20 +0100

    tests alert true/false

--- specs/cpu.async.spec.ts
+++ specs/cpu.async.spec.ts
@@ -5,18 +5,44 @@ import chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
+const THRESHOLD = 50;
+
 describe('My test suite Cpu Monitor', () => {
   it('My unit test async', async () => { 
-    let cpuMonitor = new CpuMonitor();
+    let cpuMonitor = new CpuMonitor(THRESHOLD);
+    cpuMonitor.setValue(THRESHOLD+1);
     let serviceCpuAlert = new ServiceCpuAlert(cpuMonitor);
     const result : Promise<boolean> = serviceCpuAlert.hasAlert()
         
     await expect(result).to.eventually.equal(true);
-  });
+  }),
+
+  it('should not alert', async() => {
+    let cpuMonitor = new CpuMonitor(THRESHOLD);
+    cpuMonitor.setValue(THRESHOLD-1);
+    let serviceCpuAlert = new ServiceCpuAlert(cpuMonitor);
+    const result : Promise<boolean> = serviceCpuAlert.hasAlert()
+        
+    await expect(result).to.eventually.equal(false);
+  })
+
 });
 
-class CpuMonitor {
 
+
+class CpuMonitor {
+  _threshold : number;
+  
+  _value : number;
+  
+  constructor(threshold : number) {
+    this._threshold = threshold;
+  }
+
+  setValue(value: number) {
+    this._value = value;
+  }
+  
 }
 
 class ServiceCpuAlert {
@@ -25,7 +51,6 @@ class ServiceCpuAlert {
   }
   
   constructor(cpuMon: CpuMonitor ) {
-  
     
   }
 }
