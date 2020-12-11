cdf5b06ae79c4cf54d765065b814fc4e2d7125cc
guillaume de Touloluse <yohm31@gmail.com>
2020-12-11 18:05:43 +0100

    new branch to start practicing

--- sample.async.spec.ts
+++ sample.async.spec.ts
@@ -7,7 +7,7 @@ const expect = chai.expect;
 
 describe('My test suite async', () => {
   it('My unit test async', async () => { // async keyword
-    const myNumber: Promise<number> = Promise.resolve(1); // Yeah, this is really TypeScript code!
+    const myNumber: Promise<number> = Promise.resolve(2); // Yeah, this is really TypeScript code!
     
     // Do not forget the 'await'
     await expect(myNumber).to.eventually.equal(2);
