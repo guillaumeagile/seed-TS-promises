1f4ce314617bf1261f39e772c7262717d1ddcda2
guillaume de Touloluse <yohm31@gmail.com>
2020-12-24 00:25:47 +0100

    petit tour avec conversions K/C

--- specs/sample.spec.ts
+++ specs/sample.spec.ts
@@ -23,10 +23,18 @@ describe('Sync test suite', () => {
   });
 
   it('Temparature can be increased correctly in Celcius', () => {
-    let sut: Temperature = new Temperature(0, TemperatureUnit.Celcius);; 
+    let sut: Temperature = new Temperature(0, TemperatureUnit.Celcius);
     let res = sut.Add(1);
     expect(  res.valueKelvin).to.equal(274.15);
     expect(  res.valueCelcius).to.equal(1);
+
+     // expect(  res === 274.15).to.be.true;   doesn't work in JS or TS
+     // on peut contourner le probleme avec une lib comme LodAsh
+     // lire  https://www.samanthaming.com/tidbits/33-how-to-compare-2-objects/
+
+     //on peut faire sale avec ToString()
+     let res2: Temperature = new Temperature(1, TemperatureUnit.Celcius);
+     expect(  res.toString()).to.equal(res2.toString());  //toString utilise valueOf
     
   });
 
